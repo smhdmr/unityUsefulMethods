d31d9d00a0eb3096f271b64b31b2ba061bd5dd43
smhdmr <65852878+smhdmr@users.noreply.github.com>
2021-05-22 12:35:38 +0300

    Update remapAdvanced.cs

--- remapAdvanced.cs
+++ remapAdvanced.cs
@@ -2,30 +2,29 @@
 //
 //value is the source value
 //from1-to1 is the source range and from2-to2 is the target range
-//mode 0 is for direct proportion and mode 1 is for reverse proportion
+//proportion mode is selectable
 //method returns the equivalent of the source value in the target range
 //
-public static float remapAdvanced(float value, float from1, float to1, float from2, float to2, bool mode){
+public static float RemapAdvanced(float value, float from1, float to1, float from2, float to2, bool isReverseProportion = false)
+    {
 
         float result;
-        
-        switch(mode){
-            case 0:
-            result = (to1 - from1) * (to2 - from2);
-            result = (value - from1) / result;
-            result = result + from2;
-            return result;            
 
-            case 1:
-            result = (to1 - from1) * (to2 - from2);
-            result = (value - from1) / result;
-            result = result + from2;
-            result = to2 - result + from2;
-            return result;
+        switch (isReverseProportion)
+        {
+            case false:
+                result = (to1 - from1) * (to2 - from2);
+                result = (value - from1) / result;
+                result = result + from2;
+                return result;
 
-            default:
-            return 0;   
-            
+            case true:
+                result = (to1 - from1) * (to2 - from2);
+                result = (value - from1) / result;
+                result = result + from2;
+                result = to2 - result + from2;
+                return result;            
+
         }
 
     }
