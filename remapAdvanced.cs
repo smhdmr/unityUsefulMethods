eb720e84ce0e23d78d0c5e15eab885f6c27ad647
smhdmr <65852878+smhdmr@users.noreply.github.com>
2021-05-22 12:39:02 +0300

    Update remapAdvanced.cs

--- remapAdvanced.cs
+++ remapAdvanced.cs
@@ -6,25 +6,25 @@
 //method returns the equivalent of the source value in the target range
 //
 public static float RemapAdvanced(float value, float from1, float to1, float from2, float to2, bool isReverseProportion = false)
+{
+
+    float result;
+
+    switch (isReverseProportion)
     {
+        case false:
+            result = (to1 - from1) * (to2 - from2);
+            result = (value - from1) / result;
+            result += from2;
+            return result;
 
-        float result;
-
-        switch (isReverseProportion)
-        {
-            case false:
-                result = (to1 - from1) * (to2 - from2);
-                result = (value - from1) / result;
-                result = result + from2;
-                return result;
-
-            case true:
-                result = (to1 - from1) * (to2 - from2);
-                result = (value - from1) / result;
-                result = result + from2;
-                result = to2 - result + from2;
-                return result;            
-
-        }
+        case true:
+            result = (to1 - from1) * (to2 - from2);
+            result = (value - from1) / result;
+            result += from2;
+            result = to2 - result + from2;
+            return result;            
 
     }
+
+}
